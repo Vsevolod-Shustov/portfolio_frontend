e990a028515c307f6eb1c5ab8b86aec0d54a315a
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2021-11-02 16:04:52 +0300

    animations component

---
+++ src/Animations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Animations(){
+
+  return (
+    <>
+      Animations
+    </>
+  )
+}
+
+export default Animations;
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ImageGrid from './ImageGrid.js';
+import Animations from './Animations.js';
 
 function Main(){
 
@@ -8,6 +9,7 @@ function Main(){
   return (
     <main className="block1 lg:mr-4 lg:w-9/12 lg:flex-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 auto-rows-fr">
       <ImageGrid />
+      <Animations />
     </main>
   )
 }
